3dbfc9ce2a4e7e3e634909d6042f03060dbac181
wy-20203323 <3080019161@qq.com>
2022-10-18 17:02:11 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,36 @@
+package com.coolweather.android.db;
+
+import org.litepal.exceptions.DataSupportException;
+
+public class Province extends DataSupportException {
+    public Province(String errorMessage) {
+        super(errorMessage);
+    }
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
